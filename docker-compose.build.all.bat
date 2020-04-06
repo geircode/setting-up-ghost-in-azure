e494d3649eb6bba1a21eae71f605eafbc19471fc
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-06 10:14:13 +0200

    fix scripts

--- docker-compose.build.all.bat
+++ docker-compose.build.all.bat
@@ -3,6 +3,6 @@ cd %~dp0
 set DOCKER_LATEST_VERSION_FILE=docker-19.03.4.tgz
 
 docker-compose -f filecontainer/docker-compose.yml build --no-cache
-docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml build --no-cache
 
 pause
--- docker-compose.build.up.bat
+++ docker-compose.build.up.bat
@@ -1,5 +1,8 @@
 cd %~dp0
 
+
+set DOCKER_LATEST_VERSION_FILE=docker-19.03.4.tgz
+
 call docker-compose.build.all.bat
 call docker-compose.up.bat
 
