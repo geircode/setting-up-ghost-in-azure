4b8f7408afc25df0ca93d7887199bdb89bef2145
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-10 09:54:12 +0000

    prep

--- ghost-service/docker-compose.up.sh
+++ ghost-service/docker-compose.up.sh
@@ -1,12 +1,16 @@
-cd %~dp0
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+cd $BASEDIR
+
 docker rm -f ghost_service-1
 docker-compose -f docker-compose.yml down --remove-orphans
-REM docker-compose -f docker-compose.yml pull
 
 docker network create -d overlay --attachable setting_up_ghost_in_azure_network
 
 docker-compose -f docker-compose.yml up -d --remove-orphans
-REM wait for 1-2 seconds for the container to start
-REM http://localhost:8900/
-pause
+# wait for 1-2 seconds for the container to start
+sleep 2
+# http://localhost:8900/
+
 docker exec -it ghost_service-1 /bin/bash
