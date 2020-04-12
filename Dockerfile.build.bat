fc77ab539df21588d9c24e83400397979c95231e
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-12 09:41:51 +0200

    ..

--- Dockerfile.build.bat
+++ Dockerfile.build.bat
@@ -1,3 +1,6 @@
 cd %~dp0
-docker build -f Dockerfile -t geircode/setting_up_ghost_in_azure .
+set DOCKER_LATEST_VERSION_FILE=docker-19.03.4.tgz
+
+docker build --no-cache -f Dockerfile -t geircode/setting_up_ghost_in_azure . 
+
 pause
