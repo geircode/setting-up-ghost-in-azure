a8eef81a8f84527318caa80f0b28baf7c7437a81
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-13 14:29:49 +0000

    bitnami ghost OK!

--- ghost-service/docker-compose.up.sh
+++ ghost-service/docker-compose.up.sh
@@ -10,8 +10,14 @@ docker network create -d overlay --attachable setting_up_ghost_in_azure_network
 
 docker-compose -f docker-compose.yml up -d --remove-orphans
 # wait for 1-2 seconds for the container to start
-sleep 2
+echo sleeping 5 secs
+sleep 5
 # http://localhost:8900/
 
 echo "Opening a terminal to the Ghost Container..."
 docker exec -it ghost_service-1 /bin/bash
+
+echo "### docker logs ghost_service-1"
+docker logs ghost_service-1
+echo "### docker logs mariadb"
+docker logs mariadb
